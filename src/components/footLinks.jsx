8054c8c3efd0cfd9503fa6d63f055c99e6117f63
jayteemoney <jethroirmiya@gmail.com>
2025-03-14 23:34:03 +0100

    adjusted FootLinks component

--- src/components/footLinks.jsx
+++ src/components/footLinks.jsx
@@ -1,15 +1,23 @@
 
+import { Link } from "react-router-dom";
 
 function FootLinks() {
   return (
-    <div>
-       <nav className='flex gap-[10px] font-bold justify-center bg-red-950 text-white p-[10px]'>
-          <a href="#" className="hover:text-red-700">home</a>
-          <a href="#" className="hover:text-red-700">about</a>
-          <a href="#" className="hover:text-red-700">contact</a>
-       </nav>
-    </div>
-  )
+    <footer className="bg-red-950 text-white py-3">
+      <nav className="flex justify-center gap-6 font-bold">
+        <Link to="/" className="hover:text-red-700 transition">
+          Home
+        </Link>
+        <Link to="/about" className="hover:text-red-700 transition">
+          About
+        </Link>
+        <Link to="/contact" className="hover:text-red-700 transition">
+          Contact
+        </Link>
+      </nav>
+    </footer>
+  );
 }
 
-export default FootLinks
+export default FootLinks;
+
