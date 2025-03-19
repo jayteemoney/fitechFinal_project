e81237c23d27113692e5a456d4e08c53c0b16218
jayteemoney <jethroirmiya@gmail.com>
2025-03-19 20:57:53 +0100

    removed button from landing

--- src/components/landing.jsx
+++ src/components/landing.jsx
@@ -1,12 +1,11 @@
 
 export default function Landing() {
     return (
-      <div className="text-white">
-          <div className="font-bold border border-gray-900 text-center mb-8 bg-red-950 rounded-2xl ">
+      <div className="text-white space-y-10">
+          <div className="font-bold border border-gray-900 text-center  bg-red-950 rounded-2xl ">
           <h2 className=" font-bold lg:text-[90px] ">WELCOME TO JAYS PIZZA HUB!</h2>
           <p className="text-[20px] font-bold"> Experience the best pizzas in town, made with love and the finest ingredients. Order online today!</p> 
           </div>
-          <button className="bg-red-950 rounded-[10px] p-[9px] mt-[10px]">get your now</button>  
       </div>
       
     )
